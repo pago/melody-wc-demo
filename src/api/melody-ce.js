7dfacca394975c905287d6472ba92784e382e06b
Patrick Gotthardt <patrick.gotthardt@trivago.com>
2018-01-13 20:56:49 +0100

    Minor refinements

--- src/api/melody-ce.js
+++ src/api/melody-ce.js
@@ -1,17 +1,43 @@
 import { enqueueComponent, patchInner } from 'melody-idom';
 import { createStore, applyMiddleware } from 'redux';
-import { createEpicMiddleware } from './epics';
+import { createEpicMiddleware } from 'redux-observable';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/from';
 import 'rxjs/add/observable/merge';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/startWith';
+import { animationFrame } from 'rxjs/scheduler/animationFrame.js';
 
 // base class to extend, same trick as before
 export class HTMLCustomElement extends HTMLElement {
-    constructor(_) { return (_ = super(_)).init(), _; }
-    init() { /* override as you like */ }
+    constructor(_) {
+        return (_ = super(_)).init(), _;
+    }
+    init() {
+        /* override as you like */
+    }
 }
+
+const propChanged = (name, value, oldValue, source = 'property') => ({
+    type: 'PROPERTY_CHANGED',
+    payload: {
+        name,
+        oldValue,
+        value
+    },
+    meta: {
+        source
+    }
+});
+
+const refChanged = (name, element) => ({
+    type: 'REF_CHANGED',
+    payload: {
+        name,
+        element
+    }
+});
 
 const defineProperty = element => name => {
     Object.defineProperty(element, name, {
@@ -21,17 +47,7 @@ const defineProperty = element => name => {
                 return value;
             }
             element.props[name] = value;
-            element.dispatch({
-                type: 'PROPERTY_CHANGED',
-                payload: {
-                    name,
-                    oldValue,
-                    value
-                },
-                meta: {
-                    source: 'property'
-                }
-            });
+            element.dispatch(propChanged(name, value, oldValue));
             return value;
         },
         get() {
@@ -40,19 +56,17 @@ const defineProperty = element => name => {
     });
 };
 
-const createEventSubscriber = (def, target) => {
+const createEventSubscriber = (refName, def, target) => {
     return el => {
-        const subscribers = Object.keys(def).map(
-            eventName => {
-                const handler = def[eventName];
-                return Observable.fromEvent(el, eventName).map(
-                    typeof handler === 'function'
-                        ? handler
-                        : () => handler
-                );
-            }
-        );
-        return Observable.merge(...subscribers).subscribe(action => target.dispatch(action));
+        const subscribers = Object.keys(def).map(eventName => {
+            const handler = def[eventName];
+            return Observable.fromEvent(el, eventName).map(
+                typeof handler === 'function' ? handler : () => handler
+            );
+        });
+        return Observable.merge(...subscribers)
+            .startWith(refChanged(refName, el))
+            .subscribe(action => target.dispatch(action));
     };
 };
 
@@ -60,12 +74,16 @@ const mapRefs = (refs, element) => {
     const r = typeof refs === 'object' ? refs : refs(element);
     return Object.keys(r).reduce((refs, refName) => {
         const val = r[refName];
-        refs[refName] = typeof val === 'function'
-            ? el => val(el).subscribe(action => element.dispatch(action))
-            : createEventSubscriber(val, element);
+        refs[refName] =
+            typeof val === 'function'
+                ? el =>
+                      val(el)
+                          .startWith(refChanged(refName, element))
+                          .subscribe(action => element.dispatch(action))
+                : createEventSubscriber(refName, val, element);
         return refs;
     }, {});
-}
+};
 
 const defaultReducer = (state, action) => {
     if (action.type === 'PROPERTY_CHANGED') {
@@ -74,7 +92,8 @@ const defaultReducer = (state, action) => {
     return state;
 };
 
-const initReducer = (initialState, reducer) => (state = initialState, action) => reducer(state, action);
+const initReducer = (initialState, reducer) => (state = initialState, action) =>
+    reducer(state, action);
 
 export const createElement = ({
     tagName,
@@ -88,21 +107,23 @@ export const createElement = ({
     class MelodyElement extends HTMLCustomElement {
         // Custom Element API
         init() {
-            const store = epic ? createStore(
-                initReducer(initialState, reducer),
-                applyMiddleware(createEpicMiddleware(epic))
-            ) : createStore(initReducer(initialState, reducer));
+            const store = epic
+                ? createStore(
+                      initReducer(initialState, reducer),
+                      applyMiddleware(createEpicMiddleware(epic))
+                  )
+                : createStore(initReducer(initialState, reducer));
             this.dispatch = store.dispatch;
             this.getState = store.getState;
             this.props = {};
 
             props.forEach(defineProperty(this));
 
-            Observable.from(store)
+            Observable.from(store, animationFrame)
                 // TODO: follow current shallow-equals comparison
                 .distinctUntilChanged()
                 // TODO: Integrate with scheduler
-                .subscribe(() => requestAnimationFrame(() => this.render()));
+                .subscribe(() => this.render());
 
             this.refs = mapRefs(refs, this);
         }
@@ -116,46 +137,42 @@ export const createElement = ({
             if (oldValue === value) {
                 return;
             }
-            this.dispatch({
-                type: 'PROPERTY_CHANGED',
-                payload: {
-                    name,
-                    oldValue,
-                    value
-                },
-                meta: {
-                    source: 'attribute'
-                }
-            });
+            this.dispatch(propChanged(name, value, oldValue, 'attribute'));
         }
 
         connectedCallback() {
-            props.forEach(
-                prop => {
-                    if (this.hasAttribute(prop)) {
-                        this[prop] = this.getAttribute(prop);
-                    }
+            props.forEach(prop => {
+                if (this.hasAttribute(prop)) {
+                    const value = this.getAttribute(prop);
+                    this.props[prop] = value;
+                    this.dispatch(
+                        propChanged(prop, value, undefined, 'attribute')
+                    );
                 }
-            );
+            });
             requestAnimationFrame(() => {
                 this.render();
                 this.dispatch({
-                    type: 'CONNECTED'
+                    type: 'CONNECTED',
+                    payload: this
                 });
             });
         }
 
         detachedCallback() {
             this.dispatch({
-                type: 'DETACHED'
+                type: 'DETACHED',
+                payload: this
             });
         }
 
         render() {
-            const state = {...this.getState(), ...this.refs};
+            const state = { ...this.getState(), ...this.refs };
             patchInner(this, () => render(state), state);
         }
     }
-    customElements.define(tagName, MelodyElement);
+    if (tagName) {
+        customElements.define(tagName, MelodyElement);
+    }
     return MelodyElement;
 };
--- src/counter/index.js
+++ src/counter/index.js
@@ -3,34 +3,50 @@ import { createElement } from '../api/melody-ce';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/observable/fromEvent';
 import 'rxjs/add/operator/mapTo';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/ignoreElements';
+import { combineEpics } from 'redux-observable';
 
 export default createElement({
-  tagName: 'x-counter',
-  render,
-  props: ['count'],
-  initialState: {
-    count: 0,
-    modified: false
-  },
-  refs: component => ({
-    incrementButton: el => Observable.fromEvent(el, 'click').mapTo({ type: 'INCREMENT' }),
-    decrementButton: {
-      click: { type: 'DECREMENT' }
-    }
-  }),
-  reducer(state, action) {
-    switch (action.type) {
-      case 'PROPERTY_CHANGED':
-        const { name, value, oldValue } = action.payload;
-        if (name === 'count' && !state.modified) {
-          return { count: +value };
+    tagName: 'x-counter',
+    render,
+    props: ['count'],
+    initialState: {
+        count: 0,
+        modified: false
+    },
+    refs: component => ({
+        incrementButton: el =>
+            Observable.fromEvent(el, 'click').mapTo({ type: 'INCREMENT' }),
+        decrementButton: {
+            click: { type: 'DECREMENT' }
+        }
+    }),
+    epic: combineEpics(
+        actions =>
+            actions
+                .ofType('INCREMENT')
+                .do(_ => console.log('Counter incremented'))
+                .ignoreElements(),
+        actions =>
+            actions
+                .ofType('REF_CHANGED')
+                .do(_ => console.log('refChanged ', _))
+                .ignoreElements()
+    ),
+    reducer(state, action) {
+        switch (action.type) {
+            case 'PROPERTY_CHANGED':
+                const { name, value, oldValue } = action.payload;
+                if (name === 'count' && !state.modified) {
+                    return { count: +value };
+                }
+                return state;
+            case 'INCREMENT':
+                return { count: state.count + 1, modified: true };
+            case 'DECREMENT':
+                return { count: state.count - 1, modified: true };
         }
         return state;
-      case 'INCREMENT':
-        return { count: state.count + 1, modified: true };
-      case 'DECREMENT':
-        return { count: state.count - 1, modified: true };
     }
-    return state;
-  }
 });
